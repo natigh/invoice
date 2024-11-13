ce04dccab51d53f402260fc340ca9e0ac75ef106
natigh <ntgrc1113@gmail.com>
2024-11-12 22:37:42 -0500

    clean steps user's form.

--- application/view/users/viewUsers.php
+++ application/view/users/viewUsers.php
@@ -3,30 +3,12 @@
         <div class="x_panel">
             <div class="x_title">
                 <h2>Users</h2>
-                <ul class="nav navbar-right panel_toolbox">
-                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Settings 1</a>
-                            <a class="dropdown-item" href="#">Settings 2</a>
-                        </div>
-                    </li>
-                    <li><a class="close-link"><i class="fa fa-close"></i></a>
-                    </li>
-                </ul>
                 <div class="clearfix"></div>
             </div>
             <div class="x_content">
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="card-box table-responsive">
-                            <p class="text-muted font-13 m-b-30">
-                                DataTables has most features enabled by default, so all you need to do to use it with
-                                your own tables is to call the construction function: <code>$().DataTable();</code>
-                            </p>
                             <table id="datatable" class="table table-striped table-bordered" style="width:100%">
                                 <thead>
                                     <tr>
@@ -45,18 +27,6 @@
                                 <tbody>
                                     <?php 
                                         foreach($users as $value): 
-                                        // Prepara los datos del usuario actual en un arreglo
-
-                                        $user_data = [
-                                            /*'name' => $value['name'],
-                                            'lastname' => $value['lastname'],
-                                            'typeDocument' => $value['typeDocument'],
-                                            'document' => $value['document'],
-                                            'username' => $value['username'],
-                                            'rol' => $value['rol'],
-                                            'active' => $value['active'],
-                                            'idUser' => $value['idUser'],*/
-                                        ];
                                     ?>
                                     <tr>
                                         <td><?php echo $value['name']; ?></td>
