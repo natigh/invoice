3054f3597b5b50764d7552d66f166d0165ba9a9b
natigh <ntgrc1113@gmail.com>
2024-11-12 22:37:10 -0500

    add sidebar entries, delete unused code.

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -42,6 +42,7 @@
     </script>
     <!-- jQuery -->
 
+    <script src="<?php echo URL; ?>js/invoice.js"></script>
     <script src="<?php echo URL; ?>js/user.js"></script>
     <script src="<?php echo URL; ?>js/common.js"></script>
     <script src="<?php echo URL; ?>js/sku.js"></script>
@@ -60,11 +61,15 @@
     }
 
     #objectIcon {
-      font-size: 1.5em;
+        font-size: 1.5em;
     }
 
     #btnCancel a {
         color: white;
+    }
+
+    #tableItems{
+        min-height: 500px;
     }
     </style>
 </head>
@@ -124,13 +129,17 @@
                                     </ul>
                                 </li>
                                 <li><a><span class="material-symbols-outlined">
-                                receipt_long
-                            </span> INVOICING <span class="fa fa-chevron-down"></span></a>
+                                            receipt_long
+                                        </span> INVOICING <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu">
-                                        <li><a href="<?php echo URL; ?>invoice/registerSale">Register Sales Invoice</a></li>
-                                        <li><a href="<?php echo URL; ?>invoice/registerPurchase">Register Purchase Invoice</a></li>
-                                        <li><a href="<?php echo URL; ?>invoice/viewHistorySales">History Sales Invoices</a></li>
-                                        <li><a href="<?php echo URL; ?>invoice/viewHistoryPurchases">History Purchase Invoices</a></li>
+                                        <li><a href="<?php echo URL; ?>invoice/registerSale">Register Sales Invoice</a>
+                                        </li>
+                                        <li><a href="<?php echo URL; ?>invoice/registerPurchase">Register Purchase
+                                                Invoice</a></li>
+                                        <li><a href="<?php echo URL; ?>invoice/viewHistorySales">History Sales
+                                                Invoices</a></li>
+                                        <li><a href="<?php echo URL; ?>invoice/viewHistoryPurchases">History Purchase
+                                                Invoices</a></li>
                                     </ul>
                                 </li>
                                 <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span
@@ -220,111 +229,7 @@
                 </div>
             </div>
 
-            <!-- top navigation -->
-            <div class="top_nav">
-                <div class="nav_menu">
-                    <div class="nav toggle">
-                        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                    </div>
-                    <nav class="nav navbar-nav">
-                        <ul class=" navbar-right">
-                            <li class="nav-item dropdown open" style="padding-left: 15px;">
-                                <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true"
-                                    id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                                    <img src="<?php echo URL;?>admin/images/img.jpg" alt="">John Doe
-                                </a>
-                                <div class="dropdown-menu dropdown-usermenu pull-right"
-                                    aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="javascript:;"> Profile</a>
-                                    <a class="dropdown-item" href="javascript:;">
-                                        <span class="badge bg-red pull-right">50%</span>
-                                        <span>Settings</span>
-                                    </a>
-                                    <a class="dropdown-item" href="javascript:;">Help</a>
-                                    <a class="dropdown-item" href="<?php echo URL; ?>user/logOut"><i
-                                            class="fa fa-sign-out pull-right"></i> Log Out</a>
-                                </div>
-                            </li>
-
-                            <li role="presentation" class="nav-item dropdown open">
-                                <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1"
-                                    data-toggle="dropdown" aria-expanded="false">
-                                    <i class="fa fa-envelope-o"></i>
-                                    <span class="badge bg-green">6</span>
-                                </a>
-                                <ul class="dropdown-menu list-unstyled msg_list" role="menu"
-                                    aria-labelledby="navbarDropdown1">
-                                    <li class="nav-item">
-                                        <a class="dropdown-item">
-                                            <span class="image"><img src="<?php echo URL;?>admin/images/img.jpg"
-                                                    alt="Profile Image" /></span>
-                                            <span>
-                                                <span>John Smith</span>
-                                                <span class="time">3 mins ago</span>
-                                            </span>
-                                            <span class="message">
-                                                Film festivals used to be do-or-die moments for movie makers. They were
-                                                where...
-                                            </span>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="dropdown-item">
-                                            <span class="image"><img src="<?php echo URL;?>admin/images/img.jpg"
-                                                    alt="Profile Image" /></span>
-                                            <span>
-                                                <span>John Smith</span>
-                                                <span class="time">3 mins ago</span>
-                                            </span>
-                                            <span class="message">
-                                                Film festivals used to be do-or-die moments for movie makers. They were
-                                                where...
-                                            </span>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="dropdown-item">
-                                            <span class="image"><img src="<?php echo URL;?>admin/images/img.jpg"
-                                                    alt="Profile Image" /></span>
-                                            <span>
-                                                <span>John Smith</span>
-                                                <span class="time">3 mins ago</span>
-                                            </span>
-                                            <span class="message">
-                                                Film festivals used to be do-or-die moments for movie makers. They were
-                                                where...
-                                            </span>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="dropdown-item">
-                                            <span class="image"><img src="<?php echo URL;?>admin/images/img.jpg"
-                                                    alt="Profile Image" /></span>
-                                            <span>
-                                                <span>John Smith</span>
-                                                <span class="time">3 mins ago</span>
-                                            </span>
-                                            <span class="message">
-                                                Film festivals used to be do-or-die moments for movie makers. They were
-                                                where...
-                                            </span>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <div class="text-center">
-                                            <a class="dropdown-item">
-                                                <strong>See All Alerts</strong>
-                                                <i class="fa fa-angle-right"></i>
-                                            </a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-            <!-- /top navigation -->
+            
 
             <!-- page content -->
             <div class="right_col" role="main">
