fddf0cda596f66d798e0341b961b7a14b08dd75c
natigh <ntgrc1113@gmail.com>
2024-11-12 22:38:21 -0500

    delete steps sku's form.

--- application/view/sku/registerSku.php
+++ application/view/sku/registerSku.php
@@ -2,19 +2,8 @@
     <div class="col-md-12 col-sm-12 ">
         <div class="x_panel">
             <div class="x_title">
-                <h2>Register Object</h2>
-                <ul class="nav navbar-right panel_toolbox">
-                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Settings 1</a></li>
-                            <li><a href="#">Settings 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="close-link"><i class="fa fa-close"></i></a></li>
-                </ul>
+                <h2>Register Sku</h2>
+                
                 <div class="clearfix"></div>
             </div>
             <div class="x_content">
